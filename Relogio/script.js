ba23a509aeb52586818aaa9085a78c4b7e3426d2
Gessica Vasconcelos <143418965+Gessvi@users.noreply.github.com>
2024-08-23 22:06:26 -0300

    Delete Relogio directory

--- Relogio/script.js
+++
@@ -1,9 +0,0 @@
-const value = document.querySelector('value');
-
-function onIncrement() {
-    value.innerHTML = `${parseInt(value.innerText) +1 }`;
-}
-
-function onDecrement() {
-    value.innerHTML = `${parseInt(value.innerText) - 1}`;
-}
